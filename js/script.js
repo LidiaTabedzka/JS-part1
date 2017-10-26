500f2af6e448a10555a33c09e107910d0765d7ea
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2017-10-26 07:51:52 +0200

    js/script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-var a = prompt('Enter the first number'),
-    b = prompt('Enter the second number'),
-    value;
+var a = prompt('Enter the first number');
+var b = prompt('Enter the second number');
+var value;
 
 value = (a * a) - (2 * a * b) + (b * b);
 
 console.log('The value is: ' + value);
 
-if (value > 0) {
-    console.log('wynik dodatni');
-} else if (value < 0) {
-    console.log('wynik ujemny');
-} else {
+if (value == 0) {
     console.log('wynik równy zero');
+} else {
+    value > 0 ? console.log('wynik dodatni') : console.log('wynik ujemny');
 }
